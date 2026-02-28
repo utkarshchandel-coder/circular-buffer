818a94b14fb3376182fe5e836b657b2e4be1ac29
Utkarsh Chandel <utkarshchandel3@gmail.com>
2026-02-28 21:10:12 +0530

    Improved circular buffer: added wrap-around logic, full/empty checks, and BUFFER_SIZE macro

--- circular_buffer.h
+++ circular_buffer.h
@@ -1,10 +1,11 @@
 #ifndef CIRCULAR_BUFFER_H_
 #define CIRCULAR_BUFFER_H_
+#define BUFFER_SIZE 256
 
 #include<stdint.h>
 typedef struct
 {
-uint8_t buffer[256];
+uint8_t buffer[BUFFER_SIZE];
 volatile uint8_t write_index;
 volatile uint8_t read_index;
 }circular_buffer;
@@ -13,6 +14,9 @@ void cbuffer_initialize(circular_buffer *cbuffer);
 void cbuffer_put(circular_buffer *cbuffer, uint8_t data);
 uint8_t cbuffer_get(circular_buffer *cbuffer);
 uint8_t cbuffer_data_available(circular_buffer *cbuffer);
+uint8_t cbuffer_is_empty(circular_buffer *cbuffer);
+uint8_t cbuffer_is_full(circular_buffer *cbuffer);
+
 
 #endif
 
